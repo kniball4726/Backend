babb8968bdcb108094b933375efcc445da568626
Gregory <glrd4726ml@gmail.com>
2025-12-15 19:24:27 -0300

    New Server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const server = require('./src/server/index')
 const data = require('./src/data/mongo')
 
+console.clear();
 server();
 data();
--- src/data/mongo.js
+++ src/data/mongo.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const color = require('@colors/colors')
 require('dotenv').config();
 db_uri = process.env.DB_URI
 
@@ -6,7 +7,7 @@ db_uri = process.env.DB_URI
 const data = async()=>{
     try {
         mongoose.connect(db_uri)
-        console.log("\nConectado con exito a la base de datos")    
+        console.log("\nConectado con exito a la base de datos".brightGreen)    
     } catch (error) {
         throw new Error("\nNo se puede conectar a la base de datos: ",error);
     }
